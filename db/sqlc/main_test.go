1a9cf41ef49e9108b0ab4a97ae5ec5ebe188ccad
Amirhossein Aliakbarpour <ah.aliakbarpour@gmail.com>
2022-12-24 14:49:57 +0330

    unit tests for databese CRUD

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+	"testing"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://ahaap:1245@localhost:5432/bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
